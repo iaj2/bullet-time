13b4eab3e9956045ab837ac385a3b34a029f7781
banjokido <gigandrew@shaw.ca>
2023-07-21 20:14:26 -0700

    player movement

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    public CircleCollider2D circleCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-1 * speed, rb.velocity.y);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(1 * speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 1 * speed);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -1 * speed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+    }
+}
